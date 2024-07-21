49f25d12ce7b8faad69b26026b1dfa0ece744468
thierryrudaseswa <trudaseswa@gmail.com>
2024-07-21 23:58:55 +0200

    The authentication in django python and form handling

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from myblog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myblog.urls')),
+    path('', views.home_view, name="home"),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('protected/', views.ProtectedView.as_view(), name='protected'),
 ]
--- myblog/form.py
+++ myblog/form.py
@@ -1,10 +1,29 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+# class ContactForm(forms.Form):
+#     name = forms.CharField(max_length=100)
+#     email = forms.EmailField()
+#     message = forms.CharField(widget=forms.Textarea)
 
-    def send_email(self):
-        print(f"Sending email from {self.cleaned_data['email']} with message: {self.cleaned_data['message']}")
+#     def send_email(self):
+#         print(f"Sending email from {self.cleaned_data['email']} with message: {self.cleaned_data['message']}")
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm_password")
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm']
+
+        def clean(self):
+            cleaned_data = super().clean()
+            password = cleaned_data.get('password')
+            password_confirm = cleaned_data.get('password_confirm')
+
+            # check if matches password
+            if password and password_confirm and password != password_confirm:
+                raise forms.ValidationError("passwords do not match")
+            return cleaned_data
