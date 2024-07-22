bd8bebab8c9cffbadcab933a5d6a5e273007b268
thierryrudaseswa <trudaseswa@gmail.com>
2024-07-22 13:04:07 +0200

    The full authentication in django and handling of forms

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,15 +1,11 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index)
-    # path('', views.home_view, name='home'),
-    # path('contact/', views.contact_view, name='contact'),
-    # path('contact/success', views.contact_success_view, name='contact-success')
     path('', views.home_view, name="home"),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
     path('protected/', views.ProtectedView.as_view(), name='protected'),
-    
 ]
